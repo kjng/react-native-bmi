1429d18d94fadee46c092869d3389be901a1bc8d
kjng <pianistkevinjang@gmail.com>
2017-02-18 23:44:38 -0800

    Style calculator

--- index.ios.js
+++ index.ios.js
@@ -12,9 +12,26 @@ export default class BMI extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Weight (LB)</Text>
-        <Text>Height (IN)</Text>
-        <Text>BMI</Text>
+        <View style={styles.group}>
+          <Text style={styles.label}>Weight (LB)</Text>
+          <View style={styles.inputContainer}>
+            <TextInput style={styles.input} />
+          </View>
+        </View>
+        <View style={styles.group}>
+          <Text style={styles.label}>Height (IN)</Text>
+          <View style={styles.inputContainer}>
+            <TextInput style={styles.input} />
+          </View>
+        </View>
+        <View style={[styles.group, styles.center]}>
+          <Text style={styles.bmi}>BMI: 0.00</Text>
+        </View>
+        <View style={[styles.group, styles.center]}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Compute</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -25,8 +42,37 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    backgroundColor: '#F5FCFF',
+    padding: 20
+  },
+  group: {
+    marginTop: 20,
+  },
+  inputContainer: {
+    height: 40
+  },
+  input: {
+    padding: 10,
+    flex: 1,
+    borderWidth: 1
+  },
+  button: {
+    backgroundColor: 'lightblue',
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 5
+  },
+  buttonText: {
+    fontSize: 20
+  },
+  label: {
+    fontSize: 20
+  },
+  bmi: {
+    fontSize: 20
+  },
+  center: {
+    alignItems: 'center'
   }
 });
 
