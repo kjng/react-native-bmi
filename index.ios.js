32e31a653905561a211173ca355499f96a00b892
kjng <pianistkevinjang@gmail.com>
2017-02-18 23:58:13 -0800

    Calculate BMI based on Weight and Height

--- index.ios.js
+++ index.ios.js
@@ -9,27 +9,56 @@ import {
 } from 'react-native';
 
 export default class BMI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weight: '0', // string because TextInput
+      height: '0',
+      bmi: 0
+    }
+    this.compute = this.compute.bind(this);
+  }
+
+  compute() {
+    console.log('Computing...');
+    let weight = parseFloat(this.state.weight);
+    let height = parseFloat(this.state.height);
+    let bmi = weight / Math.pow(height, 2) * 703;
+    this.setState({bmi});
+  }
+
   render() {
+    console.log(this.state);
     return (
       <View style={styles.container}>
         <View style={styles.group}>
           <Text style={styles.label}>Weight (LB)</Text>
           <View style={styles.inputContainer}>
-            <TextInput style={styles.input} />
+            <TextInput
+              keyboardType='numeric'
+              style={styles.input}
+              value={this.state.weight}
+              onChangeText={(weight) => { this.setState({weight}) }}
+            />
           </View>
         </View>
         <View style={styles.group}>
           <Text style={styles.label}>Height (IN)</Text>
           <View style={styles.inputContainer}>
-            <TextInput style={styles.input} />
+            <TextInput
+              keyboardType='numeric'
+              style={styles.input}
+              value={this.state.height}
+              onChangeText={(height) => { this.setState({height}) }}
+            />
           </View>
         </View>
         <View style={[styles.group, styles.center]}>
-          <Text style={styles.bmi}>BMI: 0.00</Text>
+          <Text style={styles.bmi}>BMI: {this.state.bmi.toFixed(2)}</Text>
         </View>
         <View style={[styles.group, styles.center]}>
           <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>Compute</Text>
+            <Text style={styles.buttonText} onPress={this.compute}>Compute</Text>
           </TouchableOpacity>
         </View>
       </View>
